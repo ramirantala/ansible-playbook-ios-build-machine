b98f4e4264f047a2c90f3adc91ca145698764905
Rami Rantala <rami.rantala@zalando.fi>
2016-03-31 16:40:21 +0300

    install pip

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 group=
 role_directory=/etc/ansible
 
+curl -O https://bootstrap.pypa.io/get-pip.py
+sudo python get-pip.py
+
 sudo pip install ansible
 if [ ! -d $role_directory ]; then
 	sudo mkdir $role_directory
